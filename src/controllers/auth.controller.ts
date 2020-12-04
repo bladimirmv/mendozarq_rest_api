6f9ff34adfe8c6bd3299b61430ec0ea57994e17e
bladimirmv <bladimilmedranoblod@gmail.com>
2020-12-04 15:17:09 -0400

    createUsuario function works right

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -20,55 +20,47 @@ function createToken(usuario: Usuario) {
 }
 
 export async function createUsuario(req: Request, res: Response) {
-
 	try {
 
 		const conn = await connect();
 		const usuario: Usuario = req.body;
 		if (!usuario.contrasenha || !usuario.username) {
 			return res.status(400).json({
-				message: 'Por favor ingrese su nombre de usuario y contrasenha.'
+				message: 'Por favor ingrese su nombre de usuario y contrasenha.',
+				data: usuario
 			});
 		}
-
 		// encrypting contrasenha
 		const salt = await bcrypt.genSalt(10);
 		const hash = await bcrypt.hash(usuario.contrasenha, salt);
 		usuario.contrasenha = hash;
 		// generate uuid
 		usuario.uuid = uuid();
-
+		// checking username
 		const finUsername = await conn.query('select username from usuario where username = ?', [usuario.username]);
 
 		if (finUsername[0].length) {
-			return res.json({
+			return res.status(400).json({
 				message: 'El nombre de usuario ya esta en uso.',
 			});
 		} else {
 			await conn.query('INSERT INTO usuario SET ?', [usuario]);
-			return res.json({
+			return res.status(201).json({
 				message: 'Usuario creado correctamente.',
 				data: usuario
 			});
 		}
-		// const compare = await bcrypt.compare(
-		// 	usuario.contrasenha,
-		// 	'$2b$10$g81WXZNIlPb6xSHBjcYmvukRKM7LEcuSGkjpfcMLnbdtu3UgJGpbK');
 	} catch (error) {
-		return res.json({
+		return res.status(400).json({
 			message: error
-
 		});
 	}
-
-
-
-
-	// const query = await conn.query('');
 }
 
 export async function login(req: Request, res: Response, next: any) {
-
+	// const compare = await bcrypt.compare(
+	// 	usuario.contrasenha,
+	// 	'$2b$10$g81WXZNIlPb6xSHBjcYmvukRKM7LEcuSGkjpfcMLnbdtu3UgJGpbK');
 	return res.send('login');
 
 }
